89e960a4daeec32ea3b6fa87ced11beccab18eeb
gupta-krishan <bpssv4335@gmail.com>
2026-03-14 13:42:33 +0530

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,25 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import MenuSection from "@/components/MenuSection";
+import GallerySection from "@/components/GallerySection";
+import ReviewsSection from "@/components/ReviewsSection";
+import OrderSection from "@/components/OrderSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <AboutSection />
+    <MenuSection />
+    <GallerySection />
+    <ReviewsSection />
+    <OrderSection />
+    <ContactSection />
+    <Footer />
+  </>
+);
+
+export default Index;
